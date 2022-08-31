e02f723b86a0116c68d61f2b828dc21be137860d
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-31 10:10:02 +0200

    feat: test analytics

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     ['meta', { name: 'twitter:site', content: '@newlogicdigital' }],
     ['meta', { name: 'theme-color', content: '#2d55ff' }],
     ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-JFTXYZ6Q63' }],
-    ['script', {}, "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-JFTXYZ6Q63');"]
+    ['script', {}, "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('consent', 'default', { 'ad_storage': 'denied', 'analytics_storage': 'denied' });\ngtag('js', new Date());\ngtag('config', 'G-JFTXYZ6Q63');"]
   ],
 
   vue: {
